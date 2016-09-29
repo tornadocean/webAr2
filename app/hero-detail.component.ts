dfb90d5fd1569eb7442e694f7ba77519c552b614
Tornadocean <smallsmilejiang@msn.com>
2016-09-29 23:24:25 +0800

    step6
    step6 finished

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core'
-
-import { Hero } from './hero'
-
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
+import { Hero } from './hero'
 import { HeroService } from './hero.service';
 
 @Component({
+    moduleId : module.id,
     selector : 'my-hero-detail',
     templateUrl: 'hero-detail.component.html',
+    styleUrls: ['hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit{

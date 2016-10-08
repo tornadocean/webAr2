c529414fb47313fa5d7dac05a448eae3e063e5c0
Tornado <smallsmilejiang@msn.com>
2016-10-08 16:57:33 +0800

    step 7.-1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail.component';
@@ -8,28 +12,30 @@ import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 import { routing }              from './app.routing';
 import { DashboardComponent }   from './dashboard.component';
+import { HeroSearchComponent }  from './hero-search.component';
+import './rxjs-extensions';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing 
     ],
   declarations: [ 
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
     ],
-  bootstrap:    [ AppComponent ],
-  providers: [HeroService]
+  bootstrap:    [ 
+    AppComponent 
+    ],
+  providers: [
+    HeroService
+    ]
 })
 
 export class AppModule { }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
